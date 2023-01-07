1bfe3100a143005dd80c17f3c28cb31c03c48422
lex0819 <lexmolnar@gmail.com>
2023-01-07 15:28:43 +0300

    added highlighting of the selected row in the currency table

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,6 +1,19 @@
+import CurrencyList from '../store/cbrf.js';
+
 function Row({ CharCode, Name, Value, Nominal, getCurrency }) {
+  const isChoicedCurrency = () => {
+    return CurrencyList.choicedCurrency === CharCode;
+  };
+
   return (
-    <tr onClick={() => getCurrency(CharCode)}>
+    <tr
+      onClick={() => getCurrency(CharCode)}
+      style={
+        isChoicedCurrency()
+          ? { backgroundColor: 'lightpink' }
+          : { backgroundColor: '' }
+      }
+    >
       <td style={{ width: 150 + 'px' }}>{Name}</td>
       <td style={{ width: 30 + ' px' }}>{CharCode}</td>
       <td style={{ width: 50 + 'px' }}>{Value}</td>
